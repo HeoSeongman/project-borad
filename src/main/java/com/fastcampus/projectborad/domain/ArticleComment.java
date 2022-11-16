74a93e61376f8d71d1b69ebd44ef44358574bfc9
HeoSeongman <tjdaks0804@naver.com>
2022-11-16 23:04:15 +0900

    AuditingFields 분리
    '@MappedSuperclass' 를 이용하여 상속 방식으로 분리시킴.
    '@DateTimeFormat' 으로 날짜/시간 형식 지정

--- src/main/java/com/fastcampus/projectborad/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectborad/domain/ArticleComment.java
@@ -16,9 +16,8 @@ import java.util.Objects;
 @Getter
 @Entity
 @ToString
-@EntityListeners(AuditingEntityListener.class)
 @Table
-public class ArticleComment {
+public class ArticleComment extends AuditingFields{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,23 +29,6 @@ public class ArticleComment {
     @Setter
     @Column(nullable = false, length = 500)
     private String content;
-
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy;
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
 
     protected ArticleComment() {}
 
