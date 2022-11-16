0707f8079b1edbeb5840c0b442c0e9eb690cf50a
HeoSeongman <tjdaks0804@naver.com>
2022-11-16 16:15:08 +0900

    create project-borad

---
+++ src/main/java/com/fastcampus/projectborad/FastCampusProjectBoradApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectborad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastCampusProjectBoradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastCampusProjectBoradApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/projectborad/FastCampusProjectBoradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.projectborad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastCampusProjectBoradApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
