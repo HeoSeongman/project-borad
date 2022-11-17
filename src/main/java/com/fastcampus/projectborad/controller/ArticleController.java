fc5a523342a1a99eb3bb57a181fffe8682f8e257
HeoSeongman <tjdaks0804@naver.com>
2022-11-17 22:30:45 +0900

    서버 사이드 렌더링을 위한 Thymeleaf 추가
    @Controller - SSR
    View 에게 보낼 데이터를 Model 이라는 객체로 전달.
    View 는 전달받은 객체로 렌더.
    클라이언트에게 html 전송.
    값을 전송하고 싶으면
    함수 반환형 앞에 @ResponseBody 를 사용
    
    @RestController - CSR
    클라이언드에게 전송하고 싶은 데이터를 JSON, XML 형식으로 값으로 전달

---
+++ src/main/java/com/fastcampus/projectborad/controller/ArticleController.java
@@ -0,0 +1,12 @@
+package com.fastcampus.projectborad.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+}
---
+++ src/test/java/com/fastcampus/projectborad/controller/ArticleControllerTest.java
@@ -0,0 +1,59 @@
+package com.fastcampus.projectborad.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 테스트 - 게시글")
+// WebMvcTest 의 값이 비어있으면 모든 컨트롤러를 불러들여 테스트를 진행한다. 값을 추가하면 해당 컨트롤러만 테스트한다.
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mockMvc;
+
+    ArticleControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("게시글 리스트 정상 호출")
+    @Test
+    public void test01() throws Exception {
+        mockMvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+                // model 에 articles 라는 속성이 있는지?
+    }
+
+    @DisplayName("게시글 한 건 정상 호출")
+    @Test
+    public void test02() throws Exception {
+        mockMvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles"));
+                // model 에 articles 라는 속성이 있는지?
+    }
+
+    @DisplayName("게시글 검색 정상 호출")
+    @Test
+    public void test03() throws Exception {
+        mockMvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+    @DisplayName("게시글 해시태그 검색 정상 호출")
+    @Test
+    public void test04() throws Exception {
+        mockMvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+}
