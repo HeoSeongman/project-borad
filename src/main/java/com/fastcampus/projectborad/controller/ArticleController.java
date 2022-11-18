42a11465c59e0aeaddbd3013f1add6472c6d8fe2
HeoSeongman <tjdaks0804@naver.com>
2022-11-18 15:12:04 +0900

    타임리프 문법으로 model 에서 view 로 넘어온 값 넘기기

--- src/main/java/com/fastcampus/projectborad/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectborad/controller/ArticleController.java
@@ -3,9 +3,13 @@ package com.fastcampus.projectborad.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RequestMapping("/articles")
@@ -16,6 +20,17 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String article(@PathVariable Long articleId, ModelMap map) {
+        map.addAttribute("article", null);
+        map.addAttribute("articleComments", List.of());
+        map.addAttribute("forwardValue", articleId);
+        map.addAttribute("writtenDate", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE));
+        map.addAttribute("writtenTime", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_TIME));
+
+        return "articles/detail";
     }
 
 }
