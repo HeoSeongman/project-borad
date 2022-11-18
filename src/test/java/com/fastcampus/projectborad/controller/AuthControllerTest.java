2d5b03a3fe167c897a922111fe54e2b50c22cdf9
HeoSeongman <tjdaks0804@naver.com>
2022-11-18 16:04:11 +0900

    스프링 시큐리티 추가 및 구성 초기 설정

---
+++ src/test/java/com/fastcampus/projectborad/controller/AuthControllerTest.java
@@ -0,0 +1,34 @@
+package com.fastcampus.projectborad.controller;
+
+import com.fastcampus.projectborad.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 인증")
+@Import(SecurityConfig.class)
+@WebMvcTest
+public class AuthControllerTest {
+
+    private final MockMvc mockMvc;
+
+    AuthControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @DisplayName("로그인 페이지 정상 호출")
+    @Test
+    public void test01() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+        // model 에 articles 라는 속성이 있는지?
+    }
+}
